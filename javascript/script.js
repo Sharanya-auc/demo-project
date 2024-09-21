50eb0b48eb3f83b10d0ff674a6ca46656a9ba78e
Sharanya <kandikondasharanya9@gmail.com>
2024-09-21 14:40:08 +0530

    style sheet is linked to html file

---
+++ javascript/script.js
@@ -0,0 +1,60 @@
+// console.log(20);
+// let num1="sharanya"
+// {
+//     let X="num1"
+//     console.log(X)
+// }
+// console.log(X)
+// var num1="sharanya"
+{
+    // let X="num1"
+    // console.log(X)
+}
+// console.log(X)
+// var num1=20
+// {
+    // let X="num1"
+    // console.log(X)
+// }
+// document.getElementById("").innerHTML = "";
+console.log("File is connected")
+
+//datatype
+
+var newname="sweety"
+{
+var newname="sharanya"
+console.log("this is using name var:", name);
+console.log("this is newname", newname);
+}
+
+console.log("outside", newname);
+
+console.log("===========")
+
+const username="user1"
+
+{
+    console.log("inside block", username)
+}
+
+// let num1=20;
+// let num2="40";
+// console.log(typeof num1);
+// console.log(typeof num2);
+// console.log(num1==num2);
+// let x=2;
+// console.log(x);
+
+
+let num1=10;
+let num2=symbol(10);
+
+let product_1{
+    img:"img_url",
+    product_name:"kjdhfkjd",
+    product_price:234.43,
+    product_rating:4
+}
+
+
